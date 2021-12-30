fc9e42d8f6fb9f5ea82b7d826982a07d37094a99
jespinal <jespinal@emeal.nttdata.com>
2021-12-30 16:35:19 -0300

    change movement and lock position when no key is pressed

--- gears-of-pong/Assets/Scripts/players/PlayerOne.cs
+++ gears-of-pong/Assets/Scripts/players/PlayerOne.cs
@@ -11,10 +11,6 @@ public class PlayerOne : MonoBehaviour
     private Vector2 velocity = new Vector2(0f, 10f);
     private Vector2 _direction = new Vector2(0f, 0f); 
 
-    private void awake() {
-        rb2D = GetComponent<Rigidbody2D>();
-    }
-
     // Start is called before the first frame update
     void Start()
     {
@@ -24,60 +20,29 @@ public class PlayerOne : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        move();
 
-        CheckInput();
+        checkInput();
     }
 
-    private void CheckInput()
+    private void checkInput()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            Debug.Log("Vector2.up !!");
             _direction = new Vector2(0f, 10f);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            Debug.Log("Vector2.down !!");
             _direction = new Vector2(0f, -10f);
+        } else 
+        {
+            _direction = new Vector2(0f, 0f);
         }
     }
 
-    void OnGUI()
-    {
-        Event e = Event.current;
-        rb2D = GetComponent<Rigidbody2D>();
-        //giveMovement(e);
-    }
-    void giveMovement(Event e) {
-        if (e.isKey)
-        {
-            Debug.Log("Detected key code: " + e.keyCode);
-            
-            KeyCode keypress = e.keyCode;
-            bool itsMoving = move(keypress);
-            if (itsMoving) {
-                Debug.Log("The player its moving !!");
-            } else {
-                Debug.Log("mmmm... the player doesn't move with that key! press A or D key, try it");
-            }
-        }
-    }
-    private void Move()
+    private void move()
     {
         rb2D.velocity = _direction * speed * Time.deltaTime;
-    }
-    bool move(KeyCode direcction) {
-        switch (direcction) {
-            case KeyCode.A:
-                rb2D.MovePosition(rb2D.position + (velocity * Time.fixedDeltaTime));
-                return true;
-            case KeyCode.D:
-                rb2D.MovePosition(rb2D.position + ((velocity * -1) * Time.fixedDeltaTime));
-                return true;
-            default:
-                return false;
-        }
     }
 
 }
