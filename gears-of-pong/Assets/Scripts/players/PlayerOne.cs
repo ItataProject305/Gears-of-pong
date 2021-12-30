2e6ce4b6df66a00edbc856bc16520beb4a11b0b9
Pablo Alvial <pablo.alvial.work@gmail.com>
2021-12-29 22:52:53 -0300

    add another type of movement

--- gears-of-pong/Assets/Scripts/players/PlayerOne.cs
+++ gears-of-pong/Assets/Scripts/players/PlayerOne.cs
@@ -3,33 +3,53 @@ using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine;
 
-public class PlayerOneScript : MonoBehaviour
+public class PlayerOne : MonoBehaviour
 {
     private BaseInput bi;
-    private Rigidbody2D _playerRigidBody;
-    public int speed = 12;
+    private Rigidbody2D rb2D;
+    private int speed = 12;
+    private Vector2 velocity = new Vector2(0f, 10f);
+    private Vector2 _direction = new Vector2(0f, 0f); 
 
     private void awake() {
-        _playerRigidBody = GetComponent<Rigidbody2D>();
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+
+        CheckInput();
     }
-    
+
+    private void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Vector2.up !!");
+            _direction = new Vector2(0f, 10f);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Vector2.down !!");
+            _direction = new Vector2(0f, -10f);
+        }
+    }
+
     void OnGUI()
     {
         Event e = Event.current;
-        giveMovement(e);
+        rb2D = GetComponent<Rigidbody2D>();
+        //giveMovement(e);
     }
-    void giveMovement() {
+    void giveMovement(Event e) {
         if (e.isKey)
         {
             Debug.Log("Detected key code: " + e.keyCode);
@@ -43,27 +63,17 @@ public class PlayerOneScript : MonoBehaviour
             }
         }
     }
-
+    private void Move()
+    {
+        rb2D.velocity = _direction * speed * Time.deltaTime;
+    }
     bool move(KeyCode direcction) {
         switch (direcction) {
             case KeyCode.A:
-                Debug.Log("Letter A is pressed");
-                float moveVertical = Input.GetAxisRaw("Vertical");
-
-                movement = new Vector2(0f, moveVertical);
-                movement = movement * speed * Time.deltaTime;
-
-                _playerRigidBody.MovePosition(transform.position + movement);
+                rb2D.MovePosition(rb2D.position + (velocity * Time.fixedDeltaTime));
                 return true;
             case KeyCode.D:
-                Debug.Log("Letter D is pressed");
-                Debug.Log("Letter A is pressed");
-                float moveVertical = Input.GetAxisRaw("Vertical");
-
-                movement = new Vector2(0f, moveVertical);
-                movement = movement * (-1 * speed) * Time.deltaTime;
-
-                _playerRigidBody.MovePosition(transform.position + movement);
+                rb2D.MovePosition(rb2D.position + ((velocity * -1) * Time.fixedDeltaTime));
                 return true;
             default:
                 return false;
